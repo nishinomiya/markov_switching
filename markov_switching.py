cc1e27b68d231abc21f2ab3ba32c6019d27b7e11
seniorpine <katsuhiko.yano@gmail.com>
2019-08-12 14:38:52 +0900

    Update markov_switching.py

--- markov_switching.py
+++ markov_switching.py
@@ -1,22 +1,26 @@
+from math import  pi
+from decimal import *
+
 def markov_switch(ts, p11, p22, regime1, regime2, err_std_dev, ergodic_probab=0.5):
-  eta = lambda x, regime, esd: 1 / sqrt(2 * pi * pow(esd, 2)) * \
-                             exp( -(pow((x - regime), 2) / (2 * pow(esd, 2))))
-  tp1000, tp0001 = exp(p11) / (1 + exp(p11)), exp(p22) / (1 + exp(p22))
-  tp0100, tp0010 = 1 - tp1000, 1 - tp0001
-  xittpo1 = np.zeros_like(ts)
-  xittpo2 = np.zeros_like(ts)
-  for i in range(ts.shape[0]):
-    eta1 = eta(ts[i], regime1, err_std_dev)
-    eta2 = eta(ts[i], regime2, err_std_dev)
-    if i == 0:
-      bt = ergodic_probab * eta1 + (1- ergodic_probab) * eta2 #ln = log(bt)
-      if bt == 0: bt=0.0001
-      xitt1 = (ergodic_probab * eta1) / bt
-      xitt2 = ((1 - ergodic_probab) * eta2) / bt
-    else:
-      bt = xittpo1[i-1] * eta1 + xittpo2[i-1] * eta2
-      xitt1 = (xittpo1[i-1] * eta1) / bt
-      xitt2 = (xittpo2[i-1] * eta2) / bt
-    xittpo1[i] = xitt1 * tp1000 + xitt2 * (1 - tp0001)
-    xittpo2[i] = xitt2 * tp0001 + xitt1 * (1 - tp1000)
+
+  dec = lambda x: Decimal(str(x))
+  eta = lambda y, regime, esd:  dec(1.0) / (dec(2.0) * dec(pi) * dec(esd)**dec(2.0)).sqrt() * \
+                  dec(-((dec(y) - dec(regime))**dec(2.0) / (dec(2.0) * dec(esd)**dec(2.0)))).exp()
+
+  tp1000, tp0001 = dec(p11).exp() / (dec(1.0)+ dec(p11).exp()), dec(p22).exp()/(dec(1.0) + dec(p22).exp())
+  tp0100, tp0010 = dec(1.0) - dec(tp1000), dec(1.0) - dec(tp0001)
+  xittpo1, xittpo2 = np.zeros_like(ts), np.zeros_like(ts)
+
+  eta1, eta2 = eta(dec(ts[0]), dec(regime1), dec(err_std_dev)), eta(dec(ts[0]), dec(regime2), dec(err_std_dev))
+  bt = dec(ergodic_probab) * dec(eta1) + (dec(1.0)- dec(ergodic_probab)) * dec(eta2)
+  xitt1,  xitt2 = (dec(ergodic_probab) * dec(eta1)) / dec(bt), ((dec(1.0) - dec(ergodic_probab)) * dec(eta2)) / dec(bt)
+  xittpo1[0] = dec(xitt1) * dec(tp1000) + dec(xitt2) * (dec(1.0) - dec(tp0001))
+  xittpo2[0] = dec(xitt2) * dec(tp0001) + dec(xitt1) * (dec(1.0) - dec(tp1000))
+
+  for i in range(1, ts.shape[0]):
+    eta1, eta2 = eta(dec(ts[i]), dec(regime1), dec(err_std_dev)), eta(dec(ts[i]), dec(regime2), dec(err_std_dev))
+    bt = dec(xittpo1[i - 1]) * dec(eta1) + dec(xittpo2[i - 1]) * dec(eta2)
+    xitt1, xitt2 = (dec(xittpo1[i - 1]) * dec(eta1)) / dec(bt), (dec(xittpo2[i-1]) * dec(eta2)) / dec(bt)
+    xittpo1[i] = dec(xitt1) * dec(tp1000) + dec(xitt2) * (dec(1.0) - dec(tp0001))
+    xittpo2[i] = dec(xitt2) * dec(tp0001) + dec(xitt1) * (dec(1.0) - dec(tp1000))
   return xittpo1
