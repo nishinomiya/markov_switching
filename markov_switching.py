ff80144a8345b1efbf03c7400fb18104679b85c5
seniorpine <katsuhiko.yano@gmail.com>
2019-08-13 05:58:35 +0900

    Update markov_switching.py

--- markov_switching.py
+++ markov_switching.py
@@ -9,15 +9,19 @@ def markov_switch(ts, p11, p22, regime1, regime2, err_std_dev, ergodic_probab=0.
     eta = lambda y, regime, esd:  dec(1.0) / (dec(2.0) * dec(pi) * dec(esd)**dec(2.0)).sqrt() * \
                   dec(-((dec(y) - dec(regime))**dec(2.0) / (dec(2.0) * dec(esd)**dec(2.0)))).exp()
 
-    tp1000, tp0001 = dec(p11).exp() / (dec(1.0)+ dec(p11).exp()), dec(p22).exp()/(dec(1.0) + dec(p22).exp())
-    tp0100, tp0010 = dec(1.0) - dec(tp1000), dec(1.0) - dec(tp0001)
+    tp1000 = dec(p11).exp() / (dec(1.0) + dec(p11).exp())
+    tp0001 = dec(p22).exp() / (dec(1.0) + dec(p22).exp())
+    tp0100 = dec(1.0) - dec(tp1000)
+    tp0010 = dec(1.0) - dec(tp0001)
     xittpo1, xittpo2 = np.zeros_like(ts), np.zeros_like(ts)
     xittpo1[-1], xittpo2[-1] =  ergodic_probab, ergodic_probab
 
     for i in range(ts.shape[0]):
-        eta1, eta2 = eta(dec(ts[i]), dec(regime1), dec(err_std_dev)), eta(dec(ts[i]), dec(regime2), dec(err_std_dev))
+        eta1 = eta(dec(ts[i]), dec(regime1), dec(err_std_dev))
+        eta2 = eta(dec(ts[i]), dec(regime2), dec(err_std_dev))
         bt = dec(xittpo1[i - 1]) * dec(eta1) + dec(xittpo2[i - 1]) * dec(eta2)
-        xitt1, xitt2 = (dec(xittpo1[i - 1]) * dec(eta1)) / dec(bt), (dec(xittpo2[i-1]) * dec(eta2)) / dec(bt)
+        xitt1 = dec(xittpo1[i - 1]) * dec(eta1) / dec(bt)
+        xitt2 = dec(xittpo2[i - 1]) * dec(eta2) / dec(bt)
         xittpo1[i] = dec(xitt1) * dec(tp1000) + dec(xitt2) * (dec(1.0) - dec(tp0001))
         xittpo2[i] = dec(xitt2) * dec(tp0001) + dec(xitt1) * (dec(1.0) - dec(tp1000))
     return xittpo1
