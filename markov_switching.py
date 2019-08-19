ac5f8982a82242fd0ce7a2001a2898b8aa3a69a6
seniorpine <katsuhiko.yano@gmail.com>
2019-08-19 15:12:21 +0900

    Update markov_switching.py

--- markov_switching.py
+++ markov_switching.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import  pi
-from decimal import *
+from decimal import Decimal
 
 def markov_switch(ts, p11, p22, regime1, regime2, err_std_div, ergodic_probab=0.5):
     dec = lambda x: Decimal(str(x))
-    eta = lambda y, regime, esd:  dec(1.0) / (dec(2.0) * dec(pi) * dec(esd)**dec(2.0)).sqrt() * \
-                  dec(-((dec(y) - dec(regime))**dec(2.0) / (dec(2.0) * dec(esd)**dec(2.0)))).exp()
+    eta = lambda y, regime, esd:  dec(1.0) / dec(2.0 * pi * esd**2.0).sqrt() * \
+                  dec(-dec((y - regime)**2.0) / (dec(2.0 * esd**2.0))).exp()
 
     tp1000 = dec(p11).exp() / (dec(1.0) + dec(p11).exp())
     tp0001 = dec(p22).exp() / (dec(1.0) + dec(p22).exp())
@@ -18,13 +18,14 @@ def markov_switch(ts, p11, p22, regime1, regime2, err_std_div, ergodic_probab=0.
     for i in range(ts.shape[0]):
         eta1 = eta(ts[i], regime1, err_std_div)
         eta2 = eta(ts[i], regime2, err_std_div)
-        bt = dec(xi_tp1[i - 1]) * dec(eta1) + dec(xi_tp2[i - 1]) * dec(eta2)
-        xi_t1 = dec(xi_tp1[i - 1]) * dec(eta1) / dec(bt)
-        xi_t2 = dec(xi_tp2[i - 1]) * dec(eta2) / dec(bt)
-        xi_tp1[i] = dec(xi_t1) * dec(tp1000) + dec(xi_t2) * (dec(1.0) - dec(tp0001))
-        xi_tp2[i] = dec(xi_t2) * dec(tp0001) + dec(xi_t1) * (dec(1.0) - dec(tp1000))
-#        xprobab2[i] = dec(xit2) * dec(tp00１0) + dec(xitt1) * (dec(1.0) - dec(tp0100))
+        bt = dec(xi_tp1[i - 1]) * eta1 + dec(xi_tp2[i - 1]) * eta2
+        xi_t1 = dec(xi_tp1[i - 1]) * eta1 / bt
+        xi_t2 = dec(xi_tp2[i - 1]) * eta2 / bt
+        xi_tp1[i] = dec(xi_t1 * tp1000) + dec(xi_t2 * (dec(1.0) - tp0001))
+        xi_tp2[i] = dec(xi_t2 * tp0001) + dec(xi_t1 * (dec(1.0) - tp1000))
+#        xi_tp2[i] = dec(xi_t2 * tp0010) + dec(xi_t1 * (dec(1.0) - tp0100))
     return xi_tp1
+
 
 def normalization(x):
     x -= np.nanmin(x)
